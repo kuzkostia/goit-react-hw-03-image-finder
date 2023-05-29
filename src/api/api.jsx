ba3a82cfe0c0f5fa321f0957ef2abc3e786a1376
kuzkostia <112246522+kuzkostia@users.noreply.github.com>
2023-05-29 18:53:39 +0300

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,5 +1,5 @@
 const KEY = '34667101-9710bd17cfb75e50a5c2acb1d';
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
 
 export const getPictures = (searchText, page) => {
   const params = new URLSearchParams({
